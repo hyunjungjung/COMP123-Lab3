7e63b63ef1c152460aaab232269d7272ab04922f
Joanne Jung <hyunjung.joannjung@outlook.com>
2016-06-10 11:48:49 -0400

    Added DisplayDeck, ShuffleDeck,Dealmethod

---
+++ COMP123-Lab3/Card.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Author : Joanne Jung
+ * Student Number : 300432364
+ * Description : Card class for Lab5
+ */
+namespace COMP123_Lab3
+{
+    public class Card
+    {
+        /**
+         * <summary>
+         * This class creates an object that represents a playing card
+         * </summary>
+         * 
+         * @class Card
+         */
+        // PUBLIC PROPERTIES +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        public int Face { get; set; }
+        public string Suit { get; set; }
+        /**
+         * <summary>
+         * This constructor takes two parameters: face and suit to instantiate a Card object
+         * </summary>
+         * 
+         * @constructor Card
+         * @param {string} face
+         * @param {string} suit
+         */
+        public Card(int face, string suit)
+        {
+            this.Face = face;
+            this.Suit = suit;
+        }
+    }
+}
--- COMP123-Lab3/Program.cs
+++ COMP123-Lab3/Program.cs
@@ -3,13 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/*
+ * Author : Joanne Jung
+ * Student Number : 300432364
+ * Description : Main method for Lab5
+ */
 namespace COMP123_Lab3
 {
-    class Program
+    /**
+     * This class is the "driver" class for our Program
+     * 
+     * @class Program
+     */
+    public class Program
     {
-        static void Main(string[] args)
+
+        /**
+         * the main method for our driver class Program
+         * 
+         * @method Main
+         * @param {string[]} args
+         */
+        public static void Main(string[] args)
         {
+            // List that represents deck of card
+
+            Deck deck = new Deck(); // create a deck of cards
+            deck.Display(); // display the initial state of the deck
+
+            deck.Shuffle(); // shuffle the deck
+            deck.Display(); // display shuffled deck
+            Console.WriteLine("=====================");
+
+            Card cardDealt = deck.Deal();
+            Console.WriteLine("Card dealt: {0} of {1}", cardDealt.Face, cardDealt.Suit);
+            Console.WriteLine();
+            deck.Display();
+
+
         }
+
+
     }
 }
